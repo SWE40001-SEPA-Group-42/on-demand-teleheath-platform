7671aad24ee5bfdf08911d1bac6ee0d312fc4728
Glaiza Cano <101419924@student.swin.edu.au>
2022-08-19 17:01:50 +1000

    :sparkles: [SEPAB-44] Create new patient form
    Added patient sign up form

---
+++ frontend/src/components/CustomFormFields/BirthSexField.tsx
@@ -0,0 +1,26 @@
+import { InputHTMLAttributes } from 'react';
+import { Field, useField } from 'formik';
+import {
+	FormControl,
+	FormErrorMessage,
+	FormLabel,
+} from '@chakra-ui/react';
+import BirthSexSelectField from './BirthSexSelectField';
+
+type Props = InputHTMLAttributes<HTMLElement> & {
+	label: string;
+	name: string;
+};
+
+const BirthSexField: React.FC<Props> = ({ label, ...props }) => {
+	const [field, meta] = useField(props);
+	return (
+		<FormControl isInvalid={Boolean(meta.error && meta.touched)} py={2}>
+			<FormLabel htmlFor={field.name}>{label}</FormLabel>
+			<Field as={BirthSexSelectField} {...field} {...props} />
+			<FormErrorMessage>{meta.error}</FormErrorMessage>
+		</FormControl>
+	);
+};
+
+export default BirthSexField;
