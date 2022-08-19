087289133e85fe403e3f93da5d9d4b15911aaf68
Thanh Nguyen <smallstarthanh01286093238@gmail.com>
2022-08-19 16:30:07 +1000

    [SEPAB-15] Update the PUT request

--- backend/server/config/db.js
+++ backend/server/config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
-const dbUrl =  "mongodb+srv://sepagroup42:sepagroup42@cluster0.t4qslyt.mongodb.net/SEPA_Group_42?retryWrites=true&w=majority"
+const dbUrl =  "mongodb+srv://sepagroup42:sepagroup42@cluster0.t4qslyt.mongodb.net/telehealthdb?retryWrites=true&w=majority"
 const connectDB = async () => {
     try {
         //const conn = await mongoose.connect(process.env.MONGO_URI)
-        
+
         const conn = await mongoose.connect(dbUrl, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
