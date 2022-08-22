e70b244536d331916036e0d49a8e7488544d6fcd
Eddieee.M <48541458+EddieeeM@users.noreply.github.com>
2022-08-22 11:56:15 +1000

    [SEPAB-8] Defined basic test, still fixing update and delete test check [WIP]

--- backend/server/config/db.js
+++ backend/server/config/db.js
@@ -1,18 +1,38 @@
 const mongoose = require('mongoose')
+const Mockgoose = require('mockgoose').Mockgoose
 
 const connectDB = async () => {
     try {
         // Please use .env file to store db connections
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
 
-        console.log(`MongoDB Connected on: ${conn.connection.host}`)
+        if (process.env.NODE_ENV == 'test') {
+
+            const mockgoose = new Mockgoose(mongoose)
+            
+            const conn = mockgoose.prepareStorage().then(()=> {
+                mongoose.connect(process.env.TEST_MONGO_URI, {
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true
+                })
+            })
+            
+        } else {
+            const conn = await mongoose.connect(process.env.MONGO_URI, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            })
+
+            console.log(`MongoDB Connected on: ${conn.connection.host}`)
+        }
+
     } catch (error) {
         console.log(error)
         process.exit(1)
     }
 }
 
-module.exports = connectDB
+const closeDB = async() => {
+    return mongoose.disconnect()
+}
+
+module.exports = { connectDB, closeDB }
