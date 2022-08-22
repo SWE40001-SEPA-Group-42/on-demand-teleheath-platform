73b1e3aecf30e3e91d333b1f91f73165891da0fa
Lemoonh <anhminhnguyenle291@gmail.com>
2022-08-22 23:56:36 +1000

    add missing fields

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import DoctorForm from './component/DoctorForm';
+import DoctorForm from './components/DoctorForm';
 
 function App() {
   return (
