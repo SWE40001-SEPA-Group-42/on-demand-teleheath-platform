01fa034386a3dcb878c7403b969daaaea6759c6d
Eddieee.M <48541458+EddieeeM@users.noreply.github.com>
2022-08-22 11:56:15 +1000

    [SEPAB-8] Defined basic test, still fixing update and delete test check [WIP]

--- backend/server/tests/endpoints.test.js
+++ backend/server/tests/endpoints.test.js
@@ -1,46 +1,36 @@
 const request = require("supertest")
-const server = require('../server');
-const mongoose = require('mongoose')
-const { clinicModel } = require('../models/userModels')
+const { clinicModel } = require('../models/userModels');
+const { app, connectDB, closeDB } = require("../server");
 
-describe('Sample Test', () => {
-  it('should test that true === true', () => {
-    expect(true).toBe(true)
-  })  
-})
+process.env.NODE_ENV = 'test'
+
+// describe('Sample Test', () => {
+//   it('should test that true === true', () => {
+//     expect(true).toBe(true)
+//   })  
+// })
+
+// Establish Connection
+connectDB()
     
 describe('Clinic Routes', () => {
 
   // Make Test Data
-  // const mockClinic = await clinicModel.create({
-  //   clinicName: "testClinic",
-  //   clinicAddress: "123 Test St",
-  //   clinicContactNumber: "04123456793",
-  //   clinicUrl: "https://test.com"
-  // })
-
-  beforeEach((done) => {
-    mongoose.connect(
-      process.env.TEST_MONGO_URI,
-      { useNewUrlParser: true },
-      () => done()
-    )
-  })
-  
-  afterEach((done) => {
-    mongoose.connection.db.dropDatabase(() => {
-      mongoose.connection.close(() => done())
-    })
-  })
+  const mockClinic = new clinicModel({
+    clinicName: "testClinic",
+    clinicAddress: "123 Test St",
+    clinicContactNumber: "04123456793",
+    clinicUrl: "https://test.com"
+  }) 
 
   describe("POST /user/clinic/", () => {
     describe("Given a clinic's details", () => {
       test("a clinic record should be created with a 200 status code", async () => {
-          const response = await request(server).post("/user/clinic/").send({
-            clinicName: "Something",
-            clinicAddress: "123 Something St",
-            clinicContactNumber: "1234567",
-            clinicUrl: "https://asdsads.com"
+          const response = await request(app).post("/user/clinic/").send({
+            clinicName: mockClinic.clinicName,
+            clinicAddress: mockClinic.clinicAddress,
+            clinicContactNumber: mockClinic.clinicContactNumber,
+            clinicUrl: mockClinic.clinicUrl
           })
           expect(response.statusCode).toBe(200)
           expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
@@ -51,8 +41,8 @@ describe('Clinic Routes', () => {
   describe("GET /user/clinic/", () => {
       describe("Given a clinic's name", () => {
         test("my searched clinic will return with a 200 status code", async () => {
-            const response = await request(server).get("/user/clinic/").send({
-              clinicName: "newTestClinic"
+            const response = await request(app).get("/user/clinic/").send({
+              clinicName: mockClinic.clinicName
             })
             expect(response.statusCode).toBe(200)
             expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
@@ -60,44 +50,31 @@ describe('Clinic Routes', () => {
       })
   })
 
-  // describe("PUT/user/clinic/:id", () => {
-  //   describe("Given a clinic's ID", () => {
-  //     test("their details will be updated with a 200 status code", async () => {
-  //         const response = await request(server).put("/user/clinic/1").send({
-  //           clinicName: "updatedTestClinic",
-  //           clinicAddress: "321 Test St",
-  //           clinicContactNumber: "04908762234",
-  //           clinicUrl: "https://test_new.com"
-  //         })
-  //         expect(response.statusCode).toBe(200)
-  //     })
-  //   })
-  // })
+  describe("PUT/user/clinic/:id", () => {
+    describe("Given a clinic's ID", () => {
+      test("their details will be updated with a 200 status code", async () => {
+        const response = await request(app).put(`/user/clinic/${updateID.id}`).send({
+          clinicName: "updatedTestClinic",
+          clinicAddress: "321 Test St",
+          clinicContactNumber: "04908762234",
+          clinicUrl: "https://test_new.com"
+        })
+          expect(response.statusCode).toBe(200)
+      })
+    })
+  })
 
-  // describe("DELETE /user/clinic/", () => {
-  //   describe("Given a clinic's details", () => {
-  //     test("a clinic record should be deleted with a 200 status code", async () => {
+  describe("DELETE /user/clinic/1", () => {
+    describe("Given a clinic's details", () => {
+      test("a clinic record should be deleted with a 200 status code", async () => {
 
-  //       await request(server)
-  //         .delete('/user/clinic/' + mockClinic.id)
-  //         .expect(200)
-  //         .then(async () => {
-  //           expect(await clinicModel.findOne({ _id: mockClinic.id })).toBeFalsy()
-  //         })
-  //     })
-  //   })
-  // })
+        await request(app)
+          .delete(`/user/clinic/${mockClinic._id}`)
+          .expect(200)
+      })
+    })
+  })
 })
 
-// `/rest/posts/${_id}`
-
-
-// test("a clinic record should be deleted with a 200 status code", async () => {
-//   const response = await request(server).delete("/user/clinic/1").send({
-//     clinicName: "newTestClinic",
-//     clinicAddress: "123 Test St",
-//     clinicContactNumber: "04123456793",
-//     clinicUrl: "https://test.com"
-//   })
-//   expect(response.statusCode).toBe(200)
-// })
+// Close Connection
+closeDB()
