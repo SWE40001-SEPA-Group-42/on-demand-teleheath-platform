c4452a9aa830aa349175e9d7139625b1e6435102
Eddieee.M <48541458+EddieeeM@users.noreply.github.com>
2022-08-24 21:05:09 +1000

    [SEPAB-8, SEPAB-21, SEPAB-23] Rework of all Routes and Controllers

---
+++ backend/server/controllers/patientController.js
@@ -0,0 +1,127 @@
+// Handles the User Object API calls to the Back End
+const asyncHandler = require('express-async-handler')
+const patientModel = require('../models/patient')
+
+// GET - Patient
+const getPatient = asyncHandler(async(req, res) => {
+
+    const patientFirstName = req.body.patientFirstName
+    const patientLastName = req.body.patientLastName
+    
+    const patient = await patientModel.find({
+        ptFirstName: patientFirstName,
+        ptLastName: patientLastName
+    })
+
+    if (!patientFirstName || !patientLastName) {
+        res.status(400)
+        throw new Error(`Invalid Patient Details: Missing inputs found in the request!`)
+    } 
+    
+    res.status(200).json(patient)
+})
+
+// POST - Patient
+const addPatient = asyncHandler(async(req, res) => {
+
+
+    const patient = await patientModel.create({
+        ptFirstName: req.body.ptFirstName,
+        ptLastName: req.body.ptLastName,
+        ptDOB: req.body.ptDOB,
+        ptGender: req.body.ptGender,
+        ptAddress: req.body.ptAddress,
+        ptReference: req.body.ptReference,
+        ptPhoneNo: req.body.ptPhoneNo,
+        ptEmail: req.body.ptEmail,
+        ptMedicareID: req.body.ptMedicareID
+    })
+
+    if (!patient) {
+        res.status(400)
+        throw new Error(`Invalid Patient Details: Missing inputs found in the request!`)
+    }
+
+    res.status(200).json(patient)
+})
+
+// PUT - Patient using ID
+const modifyPatientByID = asyncHandler(async(req, res) => {
+    const updatedPatient = await patientModel.findByIdAndUpdate(
+        req.params.id,
+        req.body, {
+            new: true,
+        }
+    )
+
+    if (!updatedPatient) {
+        res.status(400)
+        throw new Error(`Invalid Patient Search for update!`)
+    }
+
+    res.status(200).json(updatedPatient)
+})
+
+// PUT - Patient using ID
+const modifyPatientByName = asyncHandler(async(req, res) => {
+    
+    const ptFirstName = req.body.ptFirstName
+    const ptLastName = req.body.ptLastName
+    
+    const updatedPatient = await patientModel.findOneAndUpdate(
+        { 
+            ptFirstName: ptFirstName, 
+            ptLastName: ptLastName 
+        },
+        req.body, {
+            new: true,
+        }
+    )
+
+    if (!updatedPatient) {
+        res.status(400)
+        throw new Error(`Invalid Patient Search for update!`)
+    }
+
+    res.status(200).json(updatedPatient)
+})
+
+// DELETE - Patient using ID
+const deletePatientByID = asyncHandler(async(req, res) => {
+    const patient = await patientModel.findById(req.params.id)
+
+    if (!patient) {
+        res.status(400)
+        throw new Error(`Invalid Patient Search for delete!`)
+    }
+
+    await patient.remove()
+
+    res.status(200).json(patient)
+
+})
+
+// DELETE - Patient using Name
+const deletePatientByName = asyncHandler(async(req, res) => {
+    const ptFirstName = req.body.ptFirstName
+    const ptLastName = req.body.ptLastName
+    
+   
+    const patient = await patientModel.findOneAndRemove({
+        ptFirstName: ptFirstName,
+        ptLastName:  ptLastName
+    })
+
+    if (!patient) {
+        res.status(400)
+        throw new Error(`Invalid Patient Search for delete!`)
+    }
+
+    res.status(200).json(patient)
+})
+
+
+
+module.exports = {
+    getPatient, addPatient, modifyPatientByID, modifyPatientByName, deletePatientByID, deletePatientByName
+}
--- backend/server/server.js
+++ backend/server/server.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const { connectDB, closeDB } = require('./config/db')
-const userRoutes = require('./routes/userRoutes')
+// const userRoutes = require('./routes/userRoutes')
+const doctorRoutes = require('./routes/doctorRoutes')
+const patientRoutes = require('./routes/patientRoutes')
+const clinicRoutes = require('./routes/clinicRoutes')
 const { errorHandler } = require('./middleware/errorMiddleware')
 
-const usersRouter = require('./controllers/users')
-const clinicRouter = require('./controllers/clinics')
-const patientRouter = require('./controllers/users')
-const doctorRouter = require('./controllers/users')
+// const usersRouter = require('./controllers/users')
+// const clinicRouter = require('./controllers/clinics')
+// const patientRouter = require('./controllers/users')
+// const doctorRouter = require('./controllers/users')
 
 
 const port = process.env.PORT || 8001 // Default port
@@ -22,7 +25,9 @@ app.use(express.urlencoded({extended: false}))
 app.use(errorHandler)
 
 // Routes
-app.use('/user', userRoutes)
+app.use('/api/clinics', clinicRoutes)
+app.use('/api/doctors', doctorRoutes)
+app.use('/api/patients', patientRoutes)
 
 //Establish connection - TODO Move this and app.listen from the server.js to separate file (get's called multiple times in the test)
 connectDB()
@@ -31,7 +36,5 @@ app.listen(port, () => {
     console.log(`Web RTC Server initialised on port ${port}`)
 })
 
-app.use('/api/users', usersRouter)
-app.use('/api/clinics', clinicRouter)
 
 module.exports = { app, connectDB, closeDB }
