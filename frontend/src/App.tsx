e92d175e8bf623de9e5b8ee0d6263b34744abb10
Glaiza Cano <101419924@student.swin.edu.au>
2022-08-28 09:14:29 +1000

    :art: [SEPAB-31] Refactor doctor form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import DoctorForm from './containers/Doctor/DoctorForm';
 import PatientSignUp from './containers/Patient/PatientSignUp';
 import PatientAddProfile from './containers/Patient/PatientAddProfile';
 
@@ -15,6 +16,7 @@ function App() {
 							path="patient/profile/add"
 							element={<PatientAddProfile />}
 						/>
+						<Route path="doctor/add" element={<DoctorForm />} />
 					</Routes>
 				</main>
 			</div>
---
+++ frontend/src/components/CustomFormFields/LanguagesSpokenSelectField.tsx
@@ -0,0 +1,16 @@
+const LanguagesSpokenSelectField = () => {
+	return (
+		<>
+			<option value="">Select languages</option>
+			<option value="english">English</option>
+			<option value="mandarin">Mandarin</option>
+			<option value="vietnamese">Vietnamese</option>
+			<option value="hindi">Hindi</option>
+			<option value="cantonese">Cantonese</option>
+			<option value="greek">Greek</option>
+			<option value="other">Other</option>
+		</>
+	);
+};
+
+export default LanguagesSpokenSelectField;
