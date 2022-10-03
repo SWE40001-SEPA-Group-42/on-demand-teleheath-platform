53c0094d59b38bf7554d9a64266a7f1dce2bca74
Glaiza Cano <101419924@student.swin.edu.au>
2022-10-03 13:44:19 +1100

    :sparkles: [SEPAB-69] Create navigation bar
    Added navigation bar, layouts with navbar and routes

--- frontend/src/components/routing/Routing.tsx
+++ frontend/src/components/routing/Routing.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import ClinicAddProfile from '../Clinic/ClinicAddProfile';
+import ClinicUpdateProfile from '../Clinic/ClinicUpdateProfile';
 import DoctorAddProfile from '../Doctor/DoctorAddProfile';
+import DoctorUpdateProfile from '../Doctor/DoctorUpdateProfile';
 import PatientSignUp from '../Patient/PatientSignUp';
 import PatientAddProfile from '../Patient/PatientAddProfile';
+import PatientUpdateProfile from '../Patient/PatientUpdateProfile';
 import { Login, Logout, Signup, Reset } from '../Authentication';
+import LayoutsWithNavbar from '../Shared/LayoutsWithNavbar';
 import Dashboard from '../Dashboard/Dashboard';
 
 const Routing = () => {
 	return (
 		<Routes>
-			<Route index element={<PatientSignUp />} />
-			<Route path="patient/profile/add" element={<PatientAddProfile />} />
-			<Route path="doctor/profile/add" element={<DoctorAddProfile />} />
-			<Route path="clinic/profile/add" element={<ClinicAddProfile />} />
-			<Route path="login" element={<Login />} />
-			<Route path="signup" element={<Signup />} />
-			<Route path="reset" element={<Reset />} />
-			<Route path="logout" element={<Logout />} />
-			<Route index element={<Dashboard />} />
+			<Route element={<LayoutsWithNavbar />}>
+				<Route index element={<PatientSignUp />} />
+				<Route path="patient/profile/add" element={<PatientAddProfile />} />
+				<Route path="patient/profile/update" element={<PatientUpdateProfile />} />
+				<Route path="doctor/profile/add" element={<DoctorAddProfile />} />
+				<Route path="doctor/profile/update" element={<DoctorUpdateProfile />} />
+				<Route path="clinic/profile/add" element={<ClinicAddProfile />} />
+				<Route path="clinic/profile/update" element={<ClinicUpdateProfile />} />
+				<Route path="login" element={<Login />} />
+				<Route path="signup" element={<Signup />} />
+				<Route path="reset" element={<Reset />} />
+				<Route path="logout" element={<Logout />} />
+				<Route index element={<Dashboard />} />
+			</Route>
 		</Routes>
 	);
 };
