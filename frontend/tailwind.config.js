9cecb6f8d9a58201e63307aaeedd3586860ed16b
Glaiza Cano <101419924@student.swin.edu.au>
2022-10-09 21:50:01 +1100

    :wrench: [SEPAB-69] Update Chakra UI and Tailwind CSS config
    Add custom breakpoints for Chakra UI and Tailwind CSS

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'xxs': {'min': '0px', 'max': '383px'},
+      'xs': {'min': '384px', 'max': '575px'},
+      'sm': {'min': '576px', 'max': '767px'},
+      'md': {'min': '768px', 'max': '1023px'},
+      'lg': {'min': '1024px', 'max': '1279px'},
+      'xl': {'min': '1280px', 'max': '1535px'},
+      '2xl': {'min': '1536px'},
+    },
     extend: {},
   },
   plugins: [],
